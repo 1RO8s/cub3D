acb82b60f7f689eda1e4e4007b8cfb47e6746da0
kamitsui <kamitsui@student.42tokyo.jp>
2024-10-03 12:13:00 +0900

    Start impliment Feat/Texture

--- srcs/draw_3d_utils/set_wall_slice.c
+++ srcs/draw_3d_utils/set_wall_slice.c
@@ -6,11 +6,37 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 22:34:03 by kamitsui          #+#    #+#             */
-/*   Updated: 2024/09/30 18:06:01 by kamitsui         ###   ########.fr       */
+/*   Updated: 2024/10/03 11:57:48 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	debug_texture_coordinate(t_one_shot_3d *one_shot_3d)
+{
+	double	wall_x;
+	t_vector	view_point;
+	t_vector	ray_dir;
+
+	double	perp_wall_dist;
+	int		side;
+	int		fd;
+
+	fd = one_shot_3d->debug.fd;
+	perp_wall_dist = one_shot_3d->dda.perp_wall_dist;
+	view_point = one_shot_3d->player.view_point;
+	ray_dir = one_shot_3d->ray_cast.ray_dir;
+	side = one_shot_3d->dda.type_of_grid_line;
+	if (side == 0)
+		wall_x = view_point.y + perp_wall_dist * ray_dir.y;
+	else
+		wall_x = view_point.x + perp_wall_dist * ray_dir.x;
+	wall_x -= floor(wall_x);
+	dprintf(fd, "---- texture debug ----\n");
+	dprintf(fd, "\twall_x[%f]\tside[%d]\n", wall_x, side);
+	dprintf(fd, "\tside[%d]\tview_point.y[%f]\tray_dir.y[%f]\n", side, view_point.y, ray_dir.y);
+	dprintf(fd, "\tside[%d]\tview_point.x[%f]\tray_dir.x[%f]\n", side, view_point.x, ray_dir.x);
+}
 
 void	set_wall_slice(t_one_shot_3d *one_shot_3d, int x)
 {
@@ -20,6 +46,7 @@ void	set_wall_slice(t_one_shot_3d *one_shot_3d, int x)
 	t_wall_slice	wall_slice;
 
 	h = WIN_HEIGHT;
+	debug_texture_coordinate(one_shot_3d);
 	dda = one_shot_3d->dda;
 	line_height = (int)(h / dda.perp_wall_dist);
 	wall_slice.draw_start = -line_height / 2 + h / 2;
