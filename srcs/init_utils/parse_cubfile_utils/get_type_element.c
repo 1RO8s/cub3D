c170b9597ce09f6a90421593b973b3d8213c5ee9
kamitsui <kamitsui@student.42tokyo.jp>
2024-10-22 16:48:02 +0900

    Add is_texture_element function

--- srcs/init_utils/parse_cubfile_utils/get_type_element.c
+++ srcs/init_utils/parse_cubfile_utils/get_type_element.c
@@ -1,5 +1,20 @@
 #include "cub3d.h"
 
+static bool	is_texture_element(const char *str)
+{
+	const char	identifier[4] = {"NO ", "SO ", "WE ", "EA "};
+	int			i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (ft_strncmp(str, identifier[i], (size_t)3) == 0)
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
 t_enum_element	get_type_element(const char *line)
 {
 	if (is_texture_element(line) == true)
