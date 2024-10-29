119d5a8f0c51bfa1653b4d81e8bf51b83146f497
kamitsui <kamitsui@student.42tokyo.jp>
2024-10-30 03:43:18 +0900

    Fix parse_map.c
    - rename func
    - commentout unused func

--- srcs/init_utils/parse_cubfile_utils/parse_map.c
+++ srcs/init_utils/parse_cubfile_utils/parse_map.c
@@ -6,13 +6,13 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 10:31:30 by kamitsui          #+#    #+#             */
-/*   Updated: 2024/10/29 20:05:57 by kamitsui         ###   ########.fr       */
+/*   Updated: 2024/10/30 00:10:40 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	set_map_data(t_map *map, const char *map_content)
+static int	get_map_data(t_map *map, const char *map_content)
 {
 	int	count;
 
@@ -27,31 +27,31 @@ static int	set_map_data(t_map *map, const char *map_content)
 	return (EXIT_SUCCESS);
 }
 
-static char	*find_map_line(char *file_contents)
-{
-	// exist bug : 1th line of map "   111"
-	return (find_element_line(file_contents, "11"));
-}
-
-int	init_map(t_map *map, char *file_contents)
-{
-	char	*map_content;
-
-	map_content = find_map_line(file_contents);
-	if (map_content == NULL)
-	{
-		ft_dprintf(STDERR_FILENO, "Error: nonexist map\n");
-		return (EXIT_FAILURE);
-	}
-	if (is_enable_map(map_content) != true)
-	{
-		ft_dprintf(STDERR_FILENO, "Error: invalid map\n");
-		return (EXIT_FAILURE);
-	}
-	if (set_map_data(map, map_content) != EXIT_SUCCESS)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
-}
+//static char	*find_map_line(char *file_contents)
+//{
+//	// exist bug : 1th line of map "   111"
+//	return (find_element_line(file_contents, "11"));
+//}
+//
+//int	init_map(t_map *map, char *file_contents)
+//{
+//	char	*map_content;
+//
+//	map_content = find_map_line(file_contents);
+//	if (map_content == NULL)
+//	{
+//		ft_dprintf(STDERR_FILENO, "Error: nonexist map\n");
+//		return (EXIT_FAILURE);
+//	}
+//	if (is_enable_map(map_content) != true)
+//	{
+//		ft_dprintf(STDERR_FILENO, "Error: invalid map\n");
+//		return (EXIT_FAILURE);
+//	}
+//	if (set_map_data(map, map_content) != EXIT_SUCCESS)
+//		return (EXIT_FAILURE);
+//	return (EXIT_SUCCESS);
+//}
 
 bool	is_last_map(const char *line)
 {
@@ -75,9 +75,9 @@ int	parse_map(const char *line, t_parse *parse)
 		i++;
 	}
 	//printf("line[%s]\n", line);
-	if (set_map_data(&parse->game->map, line) != EXIT_SUCCESS)
+	if (get_map_data(&parse->game->map, line) != EXIT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (init_player(&parse->game->map, &parse->game->player) != EXIT_SUCCESS)
+	if (get_player_info(&parse->game->map, &parse->game->player) != EXIT_SUCCESS)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
