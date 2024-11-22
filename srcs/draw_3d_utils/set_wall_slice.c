36d014bbf57ce74bc58d99057f76e88983a9918b
kamitsui <kamitsui@student.42tokyo.jp>
2024-11-22 12:08:14 +0900

    Fix bug : too small perp_wall_distance is behavior overflow (segumentation fault)

--- srcs/draw_3d_utils/set_wall_slice.c
+++ srcs/draw_3d_utils/set_wall_slice.c
@@ -6,20 +6,26 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 22:34:03 by kamitsui          #+#    #+#             */
-/*   Updated: 2024/11/15 00:23:24 by kamitsui         ###   ########.fr       */
+/*   Updated: 2024/11/22 12:07:47 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+#define MIN_PERP_WALL_DIST 1e-4
 /**
  * @brief wall_slice calculation 
  */
 void	set_wall_slice(t_frame *frame, int x)
 {
 	t_wall_slice	wall_slice;
+	double safe_perp_wall_dist;
 
-	wall_slice.line_height = (int)(WIN_HEIGHT / frame->dda.perp_wall_dist);
+	//wall_slice.line_height = (int)(WIN_HEIGHT / frame->dda.perp_wall_dist);// overflow
+	safe_perp_wall_dist = frame->dda.perp_wall_dist;
+	if (frame->dda.perp_wall_dist < MIN_PERP_WALL_DIST)
+		safe_perp_wall_dist = MIN_PERP_WALL_DIST;
+	wall_slice.line_height = (int)(WIN_HEIGHT / safe_perp_wall_dist);
 	wall_slice.draw_start = -wall_slice.line_height / 2 + IMG_3D_HEIGHT / 2;
 	wall_slice.draw_end = wall_slice.line_height / 2 + IMG_3D_HEIGHT / 2;
 	if (wall_slice.draw_start < 0)
