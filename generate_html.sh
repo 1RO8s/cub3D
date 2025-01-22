8209ab1bea0d89065555e89335e504796695e580
kamitsui <kamitsui@student.42tokyo.jp>
2025-01-22 21:35:52 +0900

    Add script for doxygen

---
+++ generate_html.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+doxygen Doxyfile
+
+# Web browser
+#firefox html/index.html	# Linux
+#open html/index.html	# macOS
+#start html/index.html	# Windows
---
+++ preview_html.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd html
+python3 -m http.server 8000
+# Web browser
+#firefox html/index.html	# Linux
+#open html/index.html	# macOS
+#start html/index.html	# Windows
