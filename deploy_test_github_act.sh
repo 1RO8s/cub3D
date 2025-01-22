825e1aa4ed62d9bb4b4dd62f15d5c88f7cf0ecee
kamitsui <kamitsui@student.42tokyo.jp>
2025-01-22 22:17:06 +0900

    Add GitHub Actions workflow for doxygen

---
+++ deploy_test_github_act.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# dummy token
+export ACTIONS_RUNTIME_TOKEN=fake-token
+export GITHUB_TOKEN=fake-token
+
+if [[ $1 == "dry" ]]; then
+	# dryrun mode ( Only check workflow syntax )... does not docker run
+	act -n --container-architecture linux/amd64
+else
+	# Flag to ensure compatibility (My PC chip is ARM, but may require x86_64(AMD64))
+	act -j build --verbose --container-architecture linux/amd64
+fi
